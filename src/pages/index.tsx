2f581a040baf45c3171fa10a7d5fdc868bced87f
Amy Yan <ayanamydev@gmail.com>
2023-12-20 14:43:37 +1100

    feat: implemented charts for seednode resources

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,32 +2,52 @@ import type { IpGeo } from '../types';
 import * as React from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
+import { useQuery } from '@tanstack/react-query';
 import NodeList from '../components/NodeList';
 import Map from '../components/Map';
 import SeednodeList from '../components/SeednodeList';
+import ResourceChart from '../components/ResourceChart';
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
-  const [nodesGeo, setNodesGeo] = React.useState<
-    | {
-        [nodeId: string]: IpGeo;
-      }
-    | undefined
-  >();
-  const [seednodes, setSeednodes] = React.useState<{ [nodeId: string]: any }>();
-  React.useEffect(() => {
-    // Last 7 days
-    void fetch(
-      `${siteConfig.url}/api/nodes/geo?seek=${
-        Date.now() - 1000 * 60 * 60 * 24 * 7
-      }`,
-    ).then(async (data) => {
-      setNodesGeo(await data.json());
-    });
-    void fetch(`${siteConfig.url}/api/seednodes`).then(async (data) => {
-      setSeednodes(await data.json());
-    });
-  }, []);
+  const nodesGeoQuery = useQuery<{ [nodeId: string]: IpGeo }>({
+    queryKey: ['nodesGeo'],
+    queryFn: () =>
+      fetch(
+        `${siteConfig.url}/api/nodes/geo?seek=${
+          Date.now() - 1000 * 60 * 60 * 24 * 7
+        }`,
+      ).then((response) => response.json()),
+    refetchInterval: 60 * 1000,
+  });
+  const seedNodesQuery = useQuery<{ [nodeId: string]: any }>({
+    queryKey: ['seedNodes'],
+    queryFn: () =>
+      fetch(`${siteConfig.url}/api/seednodes`).then((response) =>
+        response.json(),
+      ),
+  });
+  const resourceCpuQuery = useQuery<{
+    [nodeId: string]: { values: Array<number>; timestamps: Array<number> };
+  }>({
+    queryKey: ['resourceCpu'],
+    queryFn: () =>
+      fetch(`${siteConfig.url}/api/resource/cpu`).then((response) =>
+        response.json(),
+      ),
+    refetchInterval: 60 * 1000,
+  });
+
+  const resourceMemoryQuery = useQuery<{
+    [nodeId: string]: { values: Array<number>; timestamps: Array<number> };
+  }>({
+    queryKey: ['resourceMemory'],
+    queryFn: () =>
+      fetch(`${siteConfig.url}/api/resource/memory`).then((response) =>
+        response.json(),
+      ),
+    refetchInterval: 60 * 1000,
+  });
 
   return (
     <Layout
@@ -38,15 +58,34 @@ export default function Home(): JSX.Element {
         <div className="bg-[#116466]">
           <div className="max-w-4xl mx-auto">
             <div className="px-3 py-6">
-              <Map nodesGeo={nodesGeo} />
+              <Map nodesGeo={nodesGeoQuery.data} />
             </div>
           </div>
         </div>
         <div className="max-w-2xl mx-auto space-y-3 p-3">
           <h1 className="text-2xl text-center">Seednodes:</h1>
-          {seednodes != null ? <SeednodeList seedNodes={seednodes} /> : <></>}
+          {seedNodesQuery.data != null ? (
+            <SeednodeList seedNodes={seedNodesQuery.data} />
+          ) : (
+            <></>
+          )}
+          <h1 className="text-2xl text-center">Seednodes Resources:</h1>
+          {resourceCpuQuery.data != null ? (
+            <ResourceChart data={resourceCpuQuery.data} />
+          ) : (
+            <></>
+          )}
+          {resourceMemoryQuery.data != null ? (
+            <ResourceChart data={resourceMemoryQuery.data} />
+          ) : (
+            <></>
+          )}
           <h1 className="text-2xl text-center">Nodes:</h1>
-          {nodesGeo != null ? <NodeList nodes={nodesGeo} /> : <></>}
+          {nodesGeoQuery.data != null ? (
+            <NodeList nodes={nodesGeoQuery.data} />
+          ) : (
+            <></>
+          )}
         </div>
       </div>
     </Layout>
