46440b1c3db4299b67f48745e2bbfee7a74bb911
Brynley Llewellyn-Roux <blynblack@gmail.com>
2024-09-16 08:51:44 +1000

    wip: added indirect input

--- flake.nix
+++ flake.nix
@@ -1,65 +1,46 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
-
-    nixpkgs.follows = "nixpkgs-matrix-private/nixpkgs";
     nixpkgs-matrix-private = {
-      type = "github";
-      host = "github.com";
-      owner = "MatrixAI";
-      repo = "nixpkgs-matrix-private";
+      type = "indirect";
+      id = "nixpkgs-matrix-private";
     };
- };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
-    let
-      overlays = [
-        inputs.nixpkgs-matrix-private.overlays.default
-      ];
-    in
+  outputs = { nixpkgs-matrix-private, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = overlays;
-      };
+      let
+        pkgs = nixpkgs-matrix-private.legacyPackages.${system};
 
-      shell = { ci ? false }: with pkgs; mkShell {
-        nativeBuildInputs = [
-          nodejs
-          shellcheck
-          git-lfs
-        ];
-        shellHook = ''
-          echo "Entering $(npm pkg get name)"
-          set -o allexport
-          . ./.env
-          set +o allexport
-          set -v
-          ${
-            lib.optionalString ci
-            ''
-            set -o errexit
-            set -o nounset
-            set -o pipefail
-            shopt -s inherit_errexit
-            ''
-          }
-          mkdir --parents "$(pwd)/tmp"
+        shell = { ci ? false }:
+          with pkgs;
+          mkShell {
+            nativeBuildInputs = [ nodejs shellcheck git-lfs ];
+            shellHook = ''
+              echo "Entering $(npm pkg get name)"
+              set -o allexport
+              . ./.env
+              set +o allexport
+              set -v
+              ${lib.optionalString ci ''
+                set -o errexit
+                set -o nounset
+                set -o pipefail
+                shopt -s inherit_errexit
+              ''}
+              mkdir --parents "$(pwd)/tmp"
 
-          export PATH="$(pwd)/dist/bin:$(npm root)/.bin:$PATH"
+              export PATH="$(pwd)/dist/bin:$(npm root)/.bin:$PATH"
 
-          npm install --ignore-scripts
+              npm install --ignore-scripts
 
-          set +v
-        '';
-      };
-    in
-    {
-      devShells = {
-        default = shell { ci = false; };
-        ci = shell { ci = true; };
-      };
-    });
+              set +v
+            '';
+          };
+      in {
+        devShells = {
+          default = shell { ci = false; };
+          ci = shell { ci = true; };
+        };
+      });
 }
